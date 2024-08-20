9dd6b8221b5438544a7111ecb330574114cbb19d
KaranSingh1301 <70521663+KaranSingh1301@users.noreply.github.com>
2024-08-20 22:52:05 +0530

    lecture 10

---
+++ Lecture-10 Todo 4/model/todoModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema(
+  {
+    todo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 100,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("todo", todoSchema);
